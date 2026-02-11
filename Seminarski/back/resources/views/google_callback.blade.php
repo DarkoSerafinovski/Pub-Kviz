931c0a320367ef776cc4d991f5f2c15b706f2ead
BaNbO01 <bobantodic23@gmail.com>
2026-02-11 13:47:53 +0100

    Napravljena promena nadam se da radi sada sve kako treba

--- Seminarski/back/resources/views/google_callback.blade.php
+++ Seminarski/back/resources/views/google_callback.blade.php
@@ -6,12 +6,13 @@
 <body>
     <script>
         const token = {!! $token !!};
+       const frontendUrl = "{{ env('FRONTEND_URL', 'http://localhost:3000') }}";
         if (window.opener) {
-            window.opener.postMessage({ type: 'GOOGLE_AUTH_SUCCESS', token: token }, env('FRONTEND_URL', 'http://localhost:3000'));
+            window.opener.postMessage({ type: 'GOOGLE_AUTH_SUCCESS', token: token }, frontendUrl);
             window.close();
         } else {
             localStorage.setItem('google_token', JSON.stringify(token));
-            window.location.href = env('FRONTEND_URL', 'http://localhost:3000') + "/dogadjaji";
+            window.location.href = frontendUrl + "/dogadjaji";
         }
     </script>
     <p>Autentikacija uspešna! Vraćanje u aplikaciju...</p>
